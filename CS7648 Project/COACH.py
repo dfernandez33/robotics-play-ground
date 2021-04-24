af9aeb8f93ecfdb20daa2aa03d285165439ba138
Nico Grande <nicogrande@gatech.edu>
2021-04-24 18:30:46 -0400

    Adding initial deep COACHv2 code
    Co-authored-by: Luis Pastrana <pastranakike@users.noreply.github.com>

--- CS7648 Project/COACH.py
+++ CS7648 Project/COACH.py
@@ -12,6 +12,7 @@ from pynput.mouse import Button
 import random
 from utils import calculate_reward
 import pandas as pd
+from rl_models import PolicyNetwork
 
 from language_model.model import BertTransformerVerbalReward
 
@@ -24,21 +25,36 @@ Y_MAX = 150
 Y_MIN = -28
 
 
-def train(manager: RobotManager, reward_network: RewardNetwork, loss_criterion, optimizer, starting_epoch: int,
-          epochs: int, max_length: int):
+def train(
+    manager: RobotManager,
+    reward_network: RewardNetwork,
+    policy_network: PolicyNetwork,
+    loss_criterion,
+    optimizer,
+    starting_epoch: int,
+    epochs: int,
+    max_length: int,
+    minibatch_size: int,
+    eligibility_decay: int,
+    beta: int,
+):
     global HUMAN_REWARD_SIGNAL
     global TERMINATE
     accumulated_rewards = []
-    for epoch in range(starting_epoch, starting_epoch+epochs+1):
+
+    window = []
+    eligibility_replay_buffer = []
+
+    for epoch in range(starting_epoch, starting_epoch + epochs + 1):
         if epoch % 5 == 0:
             print("Saving checkpoint...")
             torch.save(
                 {
                     "reward_network": reward_network.state_dict(),
                     "reward_optimizer": optimizer.state_dict(),
-                    "epoch": epoch
-                 },
-                f'TAMER_chkp/chkp-{epoch}.pt'
+                    "epoch": epoch,
+                },
+                f"TAMER_chkp/chkp-{epoch}.pt",
             )
         TERMINATE = False
         print("Resetting Robot Arm")
@@ -46,26 +62,55 @@ def train(manager: RobotManager, reward_network: RewardNetwork, loss_criterion, 
         print(f"Starting Epoch: {epoch}.")
         step_counter = 0
         HUMAN_REWARD_SIGNAL = 0.0
-        creditor = {}
         curr_accumulated_reward = 0
         while step_counter < max_length:
             if IS_HUMAN_TALKING:
                 time.sleep(7)
+
+            if step_counter:
+                window.append((state, best_action, p_best_action, HUMAN_REWARD_SIGNAL))
+
+            if HUMAN_REWARD_SIGNAL != 0.0:
+                eligibility_replay_buffer.append(window)
+                window = []
+                HUMAN_REWARD_SIGNAL = 0.0
+
             step_counter += 1
             state = manager.get_state()
-            reward_predictions = reward_network(state)
-            best_action = int(torch.argmax(reward_predictions).item())
-            if random.random() > .95:
-                print('RAND ACTION')
+            action_vector = policy_network(state)
+            p_best_action, best_action = torch.max(action_vector, dim=0)
+            if random.random() > 0.95:
+                print("RAND ACTION")
                 best_action = random.randint(0, 7)
-            creditor[step_counter] = (state, best_action, time.time())
-    
-            if HUMAN_REWARD_SIGNAL != 0.0:
-                update_weights(HUMAN_REWARD_SIGNAL, time.time(), creditor, loss_criterion, optimizer, reward_network)
-                HUMAN_REWARD_SIGNAL = 0.0
-                creditor = {}
+                p_best_action = action_vector[best_action]
 
-            curr_accumulated_reward += calculate_reward(TERMINATE, state, np.zeros((1, 4)))
+            if len(eligibility_replay_buffer) > minibatch_size:
+                window_sample = random.sample(eligibility_replay_buffer, minibatch_size)
+            else:
+                window_sample = eligibility_replay_buffer
+
+            eligibility_trace_bar = 0
+            for sample in window_sample:
+                eligibility_trace = 0
+                final_human_reward = sample[-1][3]
+                for state, best_action, p_best_action, _ in sample:
+                    prob_action = policy_network(state)[best_action]
+                    eligibility_trace = eligibility_decay * eligibility_trace + (
+                        prob_action / p_best_action
+                    ) * torch.log(prob_action)
+                eligibility_trace_bar += final_human_reward * eligibility_trace
+
+            eligibility_trace_bar = (
+                eligibility_trace_bar / minibatch_size
+            ) + beta * -torch.sum(
+                policy_network(state) * torch.log(policy_network(state)), axis=0
+            )
+            eligibility_trace_bar.backward()
+            optimizer.step()
+
+            curr_accumulated_reward += calculate_reward(
+                TERMINATE, state, np.zeros((1, 4))
+            )
 
             if TERMINATE:
                 print("This epoch has been aborted.")
@@ -73,31 +118,38 @@ def train(manager: RobotManager, reward_network: RewardNetwork, loss_criterion, 
 
             take_action(best_action, manager)
 
-        print(f'Accumulated_reward over epoch {epoch}: {curr_accumulated_reward}')
+        print(f"Accumulated_reward over epoch {epoch}: {curr_accumulated_reward}")
         accumulated_rewards.append(curr_accumulated_reward)
 
     return reward_network, accumulated_rewards
 
 
-def update_weights(reward_signal: float, human_time: float, creditor, loss_criterion, optimizer, reward_network):
+def update_weights(
+    reward_signal: float,
+    human_time: float,
+    creditor,
+    loss_criterion,
+    optimizer,
+    reward_network,
+):
     optimizer.zero_grad()
-    total_loss = torch.zeros((1, ))
+    total_loss = torch.zeros((1,))
     for state, best_action, action_time in creditor.values():
         reward_predictions = reward_network(state)
         print(reward_predictions)
         credit = gamma.pdf((human_time - action_time), 1, 0.0, 0.15)
         target = reward_predictions.clone()
         curr_reward = target[0, best_action]
-        mask = (target == curr_reward)
+        mask = target == curr_reward
         reward_signal = torch.ones_like(target) * reward_signal
         target = torch.where(mask, reward_signal, torch.zeros_like(reward_signal))
         print(credit * reward_predictions)
         print(target * credit)
-        print('--=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=----')
+        print("--=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=----")
         total_loss += loss_criterion(credit * reward_predictions, target * credit)
     total_loss.backward()
     optimizer.step()
-    print(f'Loss for this update: {total_loss}')
+    print(f"Loss for this update: {total_loss}")
 
 
 def take_action(action: int, manager: RobotManager):
@@ -145,7 +197,7 @@ def reward_input_handler(key):
     global HUMAN_REWARD_SIGNAL
     global TERMINATE
     global IS_HUMAN_TALKING
-    if key == KeyCode(char='s'):
+    if key == KeyCode(char="s"):
         IS_HUMAN_TALKING = True
         command = manager.transcribe_audio()
         print(command)
@@ -156,29 +208,29 @@ def reward_input_handler(key):
             HUMAN_REWARD_SIGNAL = 0
         print(HUMAN_REWARD_SIGNAL)
         IS_HUMAN_TALKING = False
-    elif key == KeyCode(char='q'):
+    elif key == KeyCode(char="q"):
         TERMINATE = True
         HUMAN_REWARD_SIGNAL = -50
-    elif key == KeyCode(char='w'):
+    elif key == KeyCode(char="w"):
         TERMINATE = True
         HUMAN_REWARD_SIGNAL = 50
-    elif key == KeyCode(char='1'):
+    elif key == KeyCode(char="1"):
         HUMAN_REWARD_SIGNAL = -2.0
-    elif key == KeyCode(char='2'):
+    elif key == KeyCode(char="2"):
         HUMAN_REWARD_SIGNAL = -1.5
-    elif key == KeyCode(char='3'):
+    elif key == KeyCode(char="3"):
         HUMAN_REWARD_SIGNAL = -1.0
-    elif key == KeyCode(char='4'):
+    elif key == KeyCode(char="4"):
         HUMAN_REWARD_SIGNAL = -0.5
-    elif key == KeyCode(char='5'):
+    elif key == KeyCode(char="5"):
         HUMAN_REWARD_SIGNAL = 0.0
-    elif key == KeyCode(char='6'):
+    elif key == KeyCode(char="6"):
         HUMAN_REWARD_SIGNAL = 0.5
-    elif key == KeyCode(char='7'):
+    elif key == KeyCode(char="7"):
         HUMAN_REWARD_SIGNAL = 1.0
-    elif key == KeyCode(char='8'):
+    elif key == KeyCode(char="8"):
         HUMAN_REWARD_SIGNAL = 1.5
-    elif key == KeyCode(char='9'):
+    elif key == KeyCode(char="9"):
         HUMAN_REWARD_SIGNAL = 2.0
 
 
@@ -190,54 +242,88 @@ def mouse_reward_handler(x, y, button, pressed):
         HUMAN_REWARD_SIGNAL = -1
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--save-data-path', help="where to save the ground truth reward data", type=str, required=True)
-    parser.add_argument('--save-model-path', help="where to save the model", type=str, required=True)
-    parser.add_argument('--checkpoint-path', type=str, default=None)
-    parser.add_argument('--language-model', type=str, default=None)
-    parser.add_argument('--num-inputs', help="number of input parameters to the networks", type=int, default=10)
-    parser.add_argument('--num-outputs', help="number of actions for the actor network", type=int, default=10)
-    parser.add_argument('--hidden-size', help="number of nodes in each hidden layer", type=int, default=256)
-    parser.add_argument('--learning-rate', type=float, default=1e-3)
-    parser.add_argument('--epochs', type=int, default=100)
-    parser.add_argument('--trajectory-length', type=int, default=100)
-    parser.add_argument('--cuda', help="Set to True in order to use GPU", type=bool, default=False)
+    parser.add_argument(
+        "--save-data-path",
+        help="where to save the ground truth reward data",
+        type=str,
+        required=True,
+    )
+    parser.add_argument(
+        "--save-model-path", help="where to save the model", type=str, required=True
+    )
+    parser.add_argument("--checkpoint-path", type=str, default=None)
+    parser.add_argument("--language-model", type=str, default=None)
+    parser.add_argument(
+        "--num-inputs",
+        help="number of input parameters to the networks",
+        type=int,
+        default=10,
+    )
+    parser.add_argument(
+        "--num-outputs",
+        help="number of actions for the actor network",
+        type=int,
+        default=10,
+    )
+    parser.add_argument(
+        "--hidden-size",
+        help="number of nodes in each hidden layer",
+        type=int,
+        default=256,
+    )
+    parser.add_argument("--learning-rate", type=float, default=1e-3)
+    parser.add_argument("--beta", type=float, default=1.5)
+    parser.add_argument("--epochs", type=int, default=100)
+    parser.add_argument("--trajectory-length", type=int, default=100)
+    parser.add_argument("--minibatch-size", type=int, default=16)
+    parser.add_argument(
+        "--cuda", help="Set to True in order to use GPU", type=bool, default=False
+    )
 
     args = parser.parse_args()
 
     ep_robot = robot.Robot()
-    ep_robot.initialize(conn_type='ap', proto_type='udp')
+    ep_robot.initialize(conn_type="ap", proto_type="udp")
     manager = RobotManager(ep_robot)
     language_model = None
-    reward_estimator = RewardNetwork(args.num_inputs, args.hidden_size, args.num_outputs)
-    optim = torch.optim.AdamW(lr=args.learning_rate, params=reward_estimator.parameters())
+    reward_estimator = RewardNetwork(
+        args.num_inputs, args.hidden_size, args.num_outputs
+    )
+    policy_network = PolicyNetwork(args.num_inputs, args.hidden_size, args.num_outputs)
+    optim = torch.optim.AdamW(
+        lr=args.learning_rate, params=reward_estimator.parameters()
+    )
     epoch = 0
     if args.checkpoint_path:
         checkpoint = torch.load(args.checkpoint_path)
         reward_estimator.load_state_dict(checkpoint["reward_network"])
-        optim.load_state_dict(checkpoint['reward_optimizer'])
-        epoch = checkpoint['epoch']
+        optim.load_state_dict(checkpoint["reward_optimizer"])
+        epoch = checkpoint["epoch"]
         print("Loaded from checkpoint: {}".format(args.checkpoint_path))
     loss = torch.nn.MSELoss()
 
-    keyboard_listener = keyboard.Listener(
-        on_press=reward_input_handler,
-    )
+    keyboard_listener = keyboard.Listener(on_press=reward_input_handler,)
     keyboard_listener.start()
 
-    mouse_listener = mouse.Listener(
-        on_click=mouse_reward_handler
-    )
+    mouse_listener = mouse.Listener(on_click=mouse_reward_handler)
     mouse_listener.start()
 
-    learned_reward, accumulated_rewards = train(manager, reward_estimator, loss, optim, epoch, args.epochs, args.trajectory_length)
+    learned_reward, accumulated_rewards = train(
+        manager,
+        reward_estimator,
+        policy_network,
+        loss,
+        optim,
+        epoch,
+        args.epochs,
+        args.trajectory_length,
+        args.minibatch_size,
+    )
 
     torch.save(
-        {
-            "reward_network": learned_reward.state_dict(),
-        },
-        f'{args.save_model_path}'
+        {"reward_network": learned_reward.state_dict(),}, f"{args.save_model_path}"
     )
     print(accumulated_rewards)
     pd.DataFrame(accumulated_rewards).to_csv(args.save_data_path)
