bfe8df15a52c4e0a91712b130c0a582b57a23096
dfernandez33 <F3rn@ndez0727>
2021-04-27 15:47:38 -0400

    TAMER OOOOPPPPP

--- CS7648 Project/TAMER_gym.py
+++ CS7648 Project/TAMER_gym.py
@@ -1,5 +1,6 @@
 from scipy.stats import gamma
 import numpy as np
+import pandas as pd
 import torch
 import time
 import gym
@@ -7,7 +8,6 @@ from pynput import keyboard
 from pynput.keyboard import KeyCode
 from collections import Counter
 import random
-from rl_models import PolicyNetwork
 from tamer_model import RewardNetwork
 
 HUMAN_REWARD_SIGNAL = 0.0
@@ -32,7 +32,9 @@ def train(
     global TERMINATE
     reward_buffer = []
     window = []
-    feedback_counter = Counter()
+    feedback_counter_positive = Counter()
+    feedback_counter_negative = Counter()
+    reward_counter = Counter()
 
     for epoch in range(0, epochs):
         TERMINATE = False
@@ -40,8 +42,11 @@ def train(
         step_counter = 0
         HUMAN_REWARD_SIGNAL = 0.0
         epoch_reward = 0
+        feedback_counter_positive[epoch] = 0
+        feedback_counter_negative[epoch] = 0
+        reward_counter[epoch] = 0
 
-        while step_counter < max_length:
+        while True:
             if TERMINATE:
                 print("This epoch has been aborted.")
                 break
@@ -75,7 +80,10 @@ def train(
                     art_states,
                 )
                 window = []
-                feedback_counter[epoch] += 1
+                if HUMAN_REWARD_SIGNAL > 0.0:
+                    feedback_counter_positive[epoch] += 1
+                else:
+                    feedback_counter_negative[epoch] += 1
                 HUMAN_REWARD_SIGNAL = 0.0
             else:
                 if len(reward_buffer) > minibatch_size:
@@ -84,19 +92,18 @@ def train(
                     window_sample = reward_buffer
                 if window_sample:
                     update_weights(
-                        window_sample, loss_criterion, optimizer, reward_network
+                        window_sample, loss_criterion, optimizer, reward_network, use_hyperball, art_states
                     )
 
             state, reward, TERMINATE, _ = env.step(best_action.item())
             epoch_reward += reward
             state = torch.from_numpy(state.astype(np.float32))
             step_counter += 1
-            time.sleep(1)
+            time.sleep(0.15)
 
-        print(f"Accumulated_reward over epoch {epoch}: {epoch_reward}")
-        print(f"Feedback over epoch {epoch}: {feedback_counter[epoch]}")
+        reward_counter[epoch] = epoch_reward
 
-    return reward_network
+    return reward_network, feedback_counter_positive, feedback_counter_negative, reward_counter
 
 
 def update_weights(
@@ -124,7 +131,7 @@ def update_weights(
                     target = torch.where(
                         mask, reward_signal, torch.zeros_like(reward_signal)
                     )
-                    total_loss += loss_criterion(reward_predictions, target) * credit
+                    total_loss += loss_criterion(reward_predictions, target) * credit * 1/art_states
     total_loss.backward()
     optimizer.step()
 
@@ -139,47 +146,28 @@ def generate_artificial_state(state, scale=0.01):
 def reward_input_handler(key):
     global HUMAN_REWARD_SIGNAL
     global TERMINATE
-    if key == KeyCode(char="q"):
-        TERMINATE = True
-        HUMAN_REWARD_SIGNAL = -50
-    elif key == KeyCode(char="w"):
-        TERMINATE = True
-        HUMAN_REWARD_SIGNAL = 50
-    elif key == KeyCode(char="1"):
-        HUMAN_REWARD_SIGNAL = -2.0
-    elif key == KeyCode(char="2"):
-        HUMAN_REWARD_SIGNAL = -1.5
-    elif key == KeyCode(char="3"):
+    if key == KeyCode(char="1"):
         HUMAN_REWARD_SIGNAL = -1.0
-    elif key == KeyCode(char="4"):
-        HUMAN_REWARD_SIGNAL = -0.5
-    elif key == KeyCode(char="5"):
-        HUMAN_REWARD_SIGNAL = 0.0
-    elif key == KeyCode(char="6"):
-        HUMAN_REWARD_SIGNAL = 0.5
-    elif key == KeyCode(char="7"):
-        HUMAN_REWARD_SIGNAL = 1.0
-    elif key == KeyCode(char="8"):
-        HUMAN_REWARD_SIGNAL = 1.5
     elif key == KeyCode(char="9"):
-        HUMAN_REWARD_SIGNAL = 2.0
+        HUMAN_REWARD_SIGNAL = 1.0
 
 
-def verify(trained_agent: PolicyNetwork, env: gym.Env):
+
+def verify(trained_agent: RewardNetwork, env: gym.Env):
     trained_agent.eval()
     reward_total = 0
-    for _ in range(10):
+    for _ in range(500):
         reward_epoch = 0
         state = env.reset()
-        for _ in range(100):
+        while True:
             action = trained_agent(torch.from_numpy(state).float()).argmax(dim=0)
             state, reward, done, _ = env.step(action.item())
             reward_epoch += reward
             if done:
-                print(f"Trial reward:{reward_epoch}")
                 reward_total += reward_epoch
                 break
-    print(f"Average Reward: {reward_total/10}")
+    print(f"Average Reward: {reward_total/500}")
+    return reward_total/500
 
 
 if __name__ == "__main__":
@@ -187,17 +175,45 @@ if __name__ == "__main__":
     nb_actions = environment.action_space.n
     nb_states = environment.observation_space.shape[0]
     hidden_state = 32
-    reward_estimator = RewardNetwork(nb_states, hidden_state, nb_actions)
-
-    optim = torch.optim.AdamW(lr=0.01, params=reward_estimator.parameters())
     loss = torch.nn.MSELoss()
 
     keyboard_listener = keyboard.Listener(on_press=reward_input_handler,)
     keyboard_listener.start()
 
     print("Training Agent")
-    train(
-        environment, reward_estimator, loss, optim, 20, 100, nb_actions,
-    )
-    print("Running Verification")
-    verify(reward_estimator, environment)
+    total_pd = pd.DataFrame()
+    total_verification_mean = []
+    for i in range(5):
+        reward_estimator = RewardNetwork(nb_states, hidden_state, nb_actions)
+        optim = torch.optim.AdamW(lr=0.005, params=reward_estimator.parameters())
+        print(f"----------------Trial {i} starting--------------")
+        reward_estimator, feedback_counter_positive, feedback_counter_negative, reward_counter = train(
+            environment, reward_estimator, loss, optim, 15, 100, nb_actions, use_hyperball=True, art_states=20,
+        )
+
+        print("Results:")
+        for epoch in feedback_counter_positive:
+            print(f'Positive feedback in epoch {epoch}: {feedback_counter_positive[epoch]} | Negative feedback in epoch {epoch}: {feedback_counter_negative[epoch]} | Reward in epoch {epoch}: {reward_counter[epoch]}')
+
+        final_pd = pd.merge(
+            pd.DataFrame(feedback_counter_positive.items()),
+            pd.DataFrame(feedback_counter_negative.items()),
+            on=0,
+            how='inner')
+
+        final_pd = pd.merge(
+            final_pd,
+            pd.DataFrame(reward_counter.items()),
+            on=0,
+            how='inner')
+
+
+        final_pd.columns = ['epoch', 'positive feedback', 'negative feedback', 'reward']
+        final_pd['trial'] = i
+        total_pd = pd.concat([total_pd, final_pd])
+        print("Running Verification")
+        total_verification_mean.append(verify(reward_estimator, environment))
+        time.sleep(1.0)
+
+    total_pd.to_csv('no_hyperball_experiment.csv')
+    pd.DataFrame(total_verification_mean).to_csv('no_hyperball_verification.csv')
